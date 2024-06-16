36ffb8608b80bf04ad4890ebb387c503675a71a4
Kelly <ekma@livsmed.com>
2024-06-17 00:32:41 +0900

    [FEAT] delete useless part and modify dashboard

--- frontend/src/components/common/footer/footer.tsx
+++ frontend/src/components/common/footer/footer.tsx
@@ -7,13 +7,10 @@ const Footer: FC<FooterProps> = () => (
   <Fragment>
     <footer className="footer mt-auto xl:ps-[15rem]  font-normal font-inter bg-white text-defaultsize leading-normal text-[0.813] shadow-[0_0_0.4rem_rgba(0,0,0,0.1)] dark:bg-bodybg py-4 text-center">
     <div className="container">
-        <span className="text-gray dark:text-defaulttextcolor/50"> Copyright © <span id="year">2024</span> <a
-                href="#" className="text-defaulttextcolor font-semibold dark:text-defaulttextcolor">Ynex</a>.
+        <span className="text-gray dark:text-defaulttextcolor/50"> Copyright © <span id="year">2024</span> .
             Designed with <span className="bi bi-heart-fill text-danger"></span> by <a href="#">
-                <span className="font-semibold text-primary underline">Spruko</span>
-            </a> All
-            rights
-            reserved
+                <span className="font-semibold text-primary underline">KELLY</span>
+            </a>
         </span>
     </div>
 </footer>
